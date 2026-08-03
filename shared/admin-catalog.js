2209308b5a4074213bd49c3bbd311f38cad6f6f5
dkdk70330-spec <dkdk70330@gmail.com>
2026-08-03 12:34:25 +0900

    Add files via upload

---
+++ shared/admin-catalog.js
@@ -0,0 +1,254 @@
+window.ADMIN_CATALOG = {
+  "profileLinkServices": [
+    {
+      "id": "x",
+      "name": "트위터",
+      "icon": "profile-links/x.png"
+    },
+    {
+      "id": "insta",
+      "name": "인스타",
+      "icon": "profile-links/insta.png"
+    },
+    {
+      "id": "slash",
+      "name": "슬래시",
+      "icon": "profile-links/slash.png"
+    },
+    {
+      "id": "lover",
+      "name": "러버미",
+      "icon": "profile-links/lover.png"
+    },
+    {
+      "id": "linktree",
+      "name": "링크트리",
+      "icon": "profile-links/linktree.png"
+    },
+    {
+      "id": "notion",
+      "name": "노션",
+      "icon": "profile-links/notion.png"
+    },
+    {
+      "id": "pushoong",
+      "name": "푸슝",
+      "icon": "profile-links/pushoong.png"
+    },
+    {
+      "id": "spin",
+      "name": "스핀",
+      "icon": "profile-links/spin.png"
+    },
+    {
+      "id": "youtube",
+      "name": "유튜브",
+      "icon": "profile-links/youtube.png"
+    },
+    {
+      "id": "bloom",
+      "name": "블룸",
+      "icon": "profile-links/bloom.png"
+    },
+    {
+      "id": "caveduck",
+      "name": "케덕",
+      "icon": "profile-links/caveduck.png"
+    },
+    {
+      "id": "rofan",
+      "name": "로판",
+      "icon": "profile-links/rofan.png"
+    },
+    {
+      "id": "rplay",
+      "name": "로플",
+      "icon": "profile-links/rplay.png"
+    },
+    {
+      "id": "tpat",
+      "name": "티팟",
+      "icon": "profile-links/tpat.png"
+    },
+    {
+      "id": "whif",
+      "name": "위프",
+      "icon": "profile-links/whif.png"
+    },
+    {
+      "id": "tingle",
+      "name": "팅글",
+      "icon": "profile-links/tingle.png"
+    },
+    {
+      "id": "zeta",
+      "name": "제타",
+      "icon": "profile-links/zeta.png"
+    },
+    {
+      "id": "melting",
+      "name": "멜팅",
+      "icon": "profile-links/melting.png"
+    }
+  ],
+  
+"platforms": [
+    {
+      "id": "bloom",
+      "name": "블룸",
+      "icon": "platforms/bloom.png"
+    },
+    {
+      "id": "caveduck",
+      "name": "케덕",
+      "icon": "platforms/caveduck.png"
+    },
+    {
+      "id": "rofan",
+      "name": "로판",
+      "icon": "platforms/rofan.png"
+    },
+    {
+      "id": "rplay",
+      "name": "로플",
+      "icon": "platforms/rplay.png"
+    },
+    {
+      "id": "tpat",
+      "name": "티팟",
+      "icon": "platforms/tpat.png"
+    },
+    {
+      "id": "whif",
+      "name": "위프",
+      "icon": "platforms/whif.png"
+    },
+
+    {
+      "id": "tingle",
+      "name": "팅글",
+      "icon": "platforms/tingle.png"
+    },
+    {
+      "id": "zeta",
+      "name": "제타",
+      "icon": "platforms/zeta.png"
+    },
+    {
+      "id": "melting",
+      "name": "멜팅",
+      "icon": "platforms/melting.png"
+    }
+  ],
+
+"genres": [
+  {
+    "id": "simulation",
+    "name": "시뮬레이션"
+  },
+  {
+    "id": "mature",
+    "name": "고수위"
+  },
+  {
+    "id": "romance",
+    "name": "로맨스"
+  },
+  {
+    "id": "fantasy",
+    "name": "판타지"
+  },
+  {
+    "id": "modern-fantasy",
+    "name": "현대판타지"
+  },
+  {
+    "id": "isekai",
+    "name": "이세계"
+  },
+  {
+    "id": "noir",
+    "name": "느와르"
+  },
+  {
+    "id": "comedy",
+    "name": "코미디"
+  },
+  {
+    "id": "healing",
+    "name": "힐링"
+  },
+  {
+    "id": "action",
+    "name": "액션"
+  },
+  {
+    "id": "horror",
+    "name": "공포"
+  },
+  {
+    "id": "adventure",
+    "name": "모험"
+  },
+  {
+    "id": "mythology",
+    "name": "신화"
+  },
+  {
+    "id": "sci-fi",
+    "name": "SF"
+  },
+  {
+    "id": "martial-arts",
+    "name": "무협"
+  },
+  {
+    "id": "eastern",
+    "name": "동양"
+  },
+  {
+    "id": "gender-swap",
+    "name": "TS물"
+  },
+  {
+    "id": "bl",
+    "name": "BL"
+  },
+  {
+    "id": "gl",
+    "name": "GL"
+  },
+  {
+    "id": "incest",
+    "name": "근친"
+  },
+  {
+    "id": "politics",
+    "name": "정치"
+  },
+  {
+    "id": "slice-of-life",
+    "name": "일상"
+  },
+  {
+    "id": "modern",
+    "name": "현대"
+  },
+  {
+    "id": "mystery",
+    "name": "미스터리"
+  },
+  {
+    "id": "omegaverse",
+    "name": "오메가버스"
+  },
+  {
+    "id": "sentinelverse",
+    "name": "센티넬버스"
+  },
+  {
+    "id": "gore",
+    "name": "고어"
+  }
+]
+};
